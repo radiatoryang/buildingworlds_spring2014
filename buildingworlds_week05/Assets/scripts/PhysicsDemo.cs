75eea7769144d67c69fea3c0e760b9edc07f86ee
Robert Yang <yang.robert.w@gmail.com>
2014-02-27 21:32:52 -0500

    added physicsDemo code ("AddForce") and levelchange code demo

---
+++ buildingworlds_week05/Assets/scripts/PhysicsDemo.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsDemo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// FixedUpdate is called __ seconds
+	void FixedUpdate () {
+		if ( Input.GetKey ( KeyCode.Space ) ) 
+		{
+			GetComponent<Rigidbody>().AddForce( transform.up * 20f, ForceMode.Acceleration );
+		}
+		
+		if ( Input.GetKey (KeyCode.UpArrow) )
+		{
+			rigidbody.AddForce ( transform.forward * 20f );
+		}
+		
+		if ( Input.GetKey (KeyCode.LeftArrow) )
+		{
+			rigidbody.AddForce ( -transform.right * 5f );
+		}
+		// rigidbody.AddForce( new Vector3(0f, 10f, 0f) );
+	}
+	
+	
+	
+}
---
+++ buildingworlds_week05/Assets/scripts/SceneChange.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		// change the scene after 60 seconds
+		if ( Time.time > 60f ) {
+			Application.LoadLevel( "dancingBaby" ); // load a scene called "dancingBaby"
+		}
+		
+		// change the scene if you press R
+		if (Input.GetKey (KeyCode.R) ) 
+		{
+			Application.LoadLevel( 1 ); // load the scene #1 in "File >> BuildSettings"
+		}
+		
+	}
+}
