bb9d6f7afa608f5ab0c8dbec2b3aa37e71759e93
Robert Yang <yang.robert.w@gmail.com>
2014-02-27 20:13:14 -0500

    added week 4 and week 5

---
+++ buildingworlds_week04/Assets/scripts/CubeMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Transform>().position += new Vector3 ( 0f, 1f, 0f );
+
+		//lowercase transform is a shortcut for "GetComponent<Transform>()"
+		// transform.position += new Vector3( 2f, 0f, -0.245f );
+
+		transform.Translate ( new Vector3( 0f, 1f, 0f ) );
+	}
+}
